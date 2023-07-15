bdaa2d5706bd3556a7607d11af829cfba4f121e3
Victor <mbarachivictor@gmail.com>
2023-07-15 18:52:13 +0100

    Setting Up Components

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "c1b334740fmsh0535a175fc2226cp1e0decjsnc335c7cef274"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/track" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
