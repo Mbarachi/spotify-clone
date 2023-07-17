fdbe7393cb179938422f712083ba9ab70a84fe28
Victor <mbarachivictor@gmail.com>
2023-07-17 23:57:08 +0100

    Creating and Styling the SideBar

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,9 +4,18 @@ import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, i, activeSong, isPlaying, data }) => {
-  const handlePauseClick = () => {};
+  const dispatch = useDispatch();
 
-  const handlePlayClick = () => {};
+  const handlePauseClick = () => {
+    console.log("working")
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    console.log("working")
+    dispatch(setActiveSong({song, data, i}));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
